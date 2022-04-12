e9a1e4ac530bdefb2dfbdd96cf9101a334b99973
Yazmin Perez <zoeperez333@gmail.com>
2022-04-11 20:34:09 -0700

    test 1-4 passing

---
+++ lib/ping_pong.rb
@@ -0,0 +1,29 @@
+require('pry')
+
+def ping_pong(number)
+  array = []
+  x = 0
+  while (x < number)
+    x = x + 1
+    if x % 3 == 0 && x % 5 == 0
+      array.push('ping pong')
+    elsif x % 3 == 0
+    array.push('ping')
+    elsif x % 5 == 0
+      array.push('pong')
+    else
+      array.push(x)
+    end
+  end
+  return array
+end
+
+
+
+
+# to check if runs cd in lib, use puts
+#  ex: puts ping_pong(25)
+
+# puts "enter a number"
+# number = gets.chomp.to_i
+#  puts ping_pong(number)
